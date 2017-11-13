9eee2f5aee4de2ee7255f79155cef159d8ae5632
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-13 21:52:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,8 @@ function frameIt() {
 
 function pressIt() {
   $('form').on('keydown', function(key){
-    if (key.which == 71)
-    alert("G was pressed.")
+    if (key.which == 71){
+      alert("G was pressed.")
+    }  
   })
 }
